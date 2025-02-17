570fb267eda96aa2729314d63bf47734d861bf9a
sarmin56952 <sarmin56952@gmail.com>
2025-02-18 00:43:12 +0600

    first commit

---
+++ pattern1_triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int row, col, n;
+  scanf("%d", &n);
+  for (row = 1; row <= n; row++)
+  {
+    for (col = 1; col <= row; col++)
+      printf(" %c", row + 64);
+    printf("\n");
+  }
+  return 0;
+}
