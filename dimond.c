b8bc41e3fefca81b0b33f0a5b37338542b5d38ae
sarmin56952 <sarmin56952@gmail.com>
2025-02-17 16:42:49 +0600

    first commit

---
+++ dimond.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+  int n, row, col;
+  scanf("%d", &n);
+  for (row = 1; row <= n; row++)
+  {
+    for (col = 1; col <= n - row; col++)
+      printf(" ");
+    for (col = 1; col <= 2 * row - 1; col++)
+      if (col == 1 || col == (2 * row - 1))
+        printf("*");
+      else
+        printf(" ");
+    printf("\n");
+  }
+  for (row = n - 1; row >= 1; row--)
+  {
+    for (col = 1; col <= n - row; col++)
+      printf(" ");
+    for (col = 1; col <= 2 * row - 1; col++)
+      if (col == 1 || col == (2 * row - 1))
+        printf("*");
+      else
+        printf(" ");
+    printf("\n");
+  }
+  return 0;
+}
