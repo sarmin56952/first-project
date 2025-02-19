8b24d56f78c8266169ce6ebc278112fb9c681f53
sarmin56952 <sarmin56952@gmail.com>
2025-02-19 22:32:49 +0600

    first commit

---
+++ pattern5ractangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+  int n, row, col;
+  scanf("%d", &n);
+  for (row = 1; row <= n; row++)
+  {
+    for (col = 1; col <= n; col++)
+    {
+      if (col == 1 || col == n || row == 1 || row == n)
+        printf("* ");
+      else
+        printf("  ");
+    }
+    printf("\n");
+  }
+  return 0;
+}
+/*
+6
+* * * * * *
+*         *
+*         *
+*         *
+*         *
+* * * * * *
+*/
